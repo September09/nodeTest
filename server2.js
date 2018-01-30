6a598bd56a4d270a500c5c2593c6644417a8b89f
Jiuyue Jia (贾九月) <jiuyuejia@administratordeiMac-2.local>
2018-01-30 16:54:44 +0800

    update code

---
+++ server2.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+app.get('/index.html', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/process_post', urlencodedParser, (req, res) => {
+    const response = {
+        'first_name': req.body.first_name,
+        'last_name': req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+
+});
+
+const server = app.listen(8081, () => {
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+});
